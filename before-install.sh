1b512bad6f3945a8c407c40069c1cf1bd231fac7
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-03-24 20:16:10 +1000

    fixed travis yml

--- before-install.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.local/bin
-export PATH=$HOME/.local/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'

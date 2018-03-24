ea1ab91a5c60b2e3f92c586b827b014ebd97e87e
Anis Jonischkeit <anis.jonischkeit@gmail.com>
2018-03-24 20:20:53 +1000

    try without stack using http://timbaumann.info/posts/2013-08-04-hakyll-github-and-travis.html tutorial

--- compile.sh
+++ compile.sh
@@ -3,6 +3,9 @@
 set -e # Exit with nonzero exit code if anything fails
 
 echo "hellos"
-stack build
-stack exec site build
+cabal install hakyll
+ghc --make site.hs
+./site build
+# stack build
+# stack exec site build
 ls
